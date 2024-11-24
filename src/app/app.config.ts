bb2f0af85d23371ded7d6f6fcd1a91a1a4e0cacf
Said Chibane <saidrideau@gmail.com>
2024-11-23 21:18:44 -0500

    4 - fix bug

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,10 +5,12 @@ import { APP_ROUTES } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
 import { TranslateModule } from '@ngx-translate/core';
 import { I18N_CONFIG } from './configs/translation.config';
+import { MessageService } from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(APP_ROUTES),
     importProvidersFrom(HttpClientModule, TranslateModule.forRoot(I18N_CONFIG)),
+    MessageService,
   ],
 };
