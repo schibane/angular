16e7843b0133e731395cd0f2287c61fce4768014
Said Chibane <saidrideau@gmail.com>
2024-11-28 20:40:37 -0500

    19

--- src/app/pages/forms/user-form/user-form.component.ts
+++ src/app/pages/forms/user-form/user-form.component.ts
@@ -1,12 +1,36 @@
+// user-form.component.ts
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './user-form.component.html',
-  styleUrl: './user-form.component.css'
+  styleUrls: ['./user-form.component.css'],
 })
 export class UserFormComponent {
+  userForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      console.log(this.userForm.value);
+    } else {
+      console.error('Form is invalid');
+    }
+  }
 }
